60796913c6a57c835d61c4365aa5db856d7518da
Andreas Farre <andreas.farre@gmail.com>
2014-07-09 20:25:15 +0200

    Polish VarArgs a bit.

--- VarArgs.hs
+++ VarArgs.hs
@@ -1,5 +1,13 @@
-{-# Language TypeFamilies, RankNTypes #-}
+{-# Language TypeFamilies #-}
 module VarArgs where
+
+newtype List a = List ([a] -> [a])
+
+append :: a -> List a -> List a
+append x (List l) = List (l . (x :))
+
+list :: List a -> [a]
+list (List l) = l []
 
 {-
 This is Oleg Kiselyov's polyvariadic functions in Haskell but using
@@ -8,15 +16,15 @@ http://okmij.org/ftp/Haskell/polyvariadic.html#polyvar-fn
 -}
 class VarArgs r where
   type Elem r
-  build   :: [Elem r] -> Elem r -> r
+  build :: List (Elem r) -> r
 
-args :: forall r a . (VarArgs r, Elem r ~ a) => a -> r
-args x = build [] x
+args :: VarArgs t => Elem t -> t
+args = build (List id)
 
-instance VarArgs [a] where
-  type Elem [a] = a
-  build l x = reverse $ x:l
+instance VarArgs (List a) where
+  type Elem (List a) = a
+  build l = l
 
 instance (VarArgs r, Elem r ~ a) => VarArgs (a -> r) where
   type Elem (a -> r) = a
-  build l x y = build (x:l) y
+  build l x = build (append x l)
