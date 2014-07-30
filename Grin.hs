8b6516578376758618c31ff1a7a54f27d5006124
Andreas Farre <andreas.farre@gmail.com>
2014-07-30 19:26:53 +0200

    Move test to a separate file.

--- Grin.hs
+++ Grin.hs
@@ -1,7 +1,11 @@
 {-# Language GADTs, NoMonomorphismRestriction, NoMonoLocalBinds,
              RankNTypes, TypeSynonymInstances, FlexibleInstances,
              RecordWildCards  #-}
-module Grin where
+module Grin (
+  module Grin,
+  module Syntax,
+  module VarArgs
+) where
 
 import Control.Monad.ST.Lazy
 import Data.Function
@@ -9,7 +13,6 @@ import Data.Function
 import List
 import Program
 import Syntax
-import Pretty
 import Unique
 import VarArgs
 
@@ -114,97 +117,3 @@ instance (Pattern a, Declarable b) => Declarable (a -> b) where
 
 declare :: Declarable d => d -> Declaration
 declare d = runST (newSupply 0 (+1) >>= \s -> buildDeclaration empty s d)
-
--- TODO(farre): Remove testing, start using QuickCheck!
-test :: Pattern a => Integer -> Grin a
-test n = do
-  Var v <- unit n
-  unit v
-
-test' :: Integer -> Grin GrinValue
-test' n = unit n >>= \(Var v) -> return (toValue v)
-
-test2 :: Integer -> Integer -> Grin GrinValue
-test2 m n = do
-  Var v <- unit m
-  Var w <- unit n
-  Variable x <- unit (99 :: Integer)
-  Var q <- unit w
-  return (Variable x)
-
-test3 :: Pattern a => Grin a
-test3 = do
-  Foo v0 v1 <- unit (Node "foo" [(number 3), (number 8)])
-  Var v2 <- unit v1
-  Foo v3 v4 <- unit (42 :: Integer)
-  Var v5 <- unit v4
-  unit v0
-
-test4 :: Pattern a => Grin a
-test4 = do
-  Var x <- test (5 :: Integer)
-  Var y <- test3
-  unit y
-
-test5 :: Pattern a => Grin a
-test5 = do
-  Var x <- unit (5 :: Integer)
-  Var y <- store x
-  Var z <- update y (5 :: Integer)
-  unit y
-
-test4' :: Pattern a => Grin a
-test4' = test 5 >>= \(Var x) -> test3 >>= \(Var y) -> unit y
-
-test6 :: Pattern a => Grin a
-test6 = do
-  Var x <- unit (5 :: Integer)
-  Var y <- store x
-  Var z <- (Name "f") $+ args (toValue x) (toValue y) (number 5)
-  unit z
-
-test7 :: Pattern a => Grin a
-test7 = do
-  Var x <- unit (5 :: Integer)
-  Var y <- switch x $ args
-             (match $ \(Var x) -> unit x)
-             (match $ \(Foo a b) -> unit x)
-  unit y
-
-test8 :: Pattern a => Var -> Grin a
-test8 (Var f) = do
-  Var x <- switch f $ args
-             (match $ \(Foo a b) -> unit a)
-  unit x
-
-test9 :: Pattern a => Var -> Var -> Grin a
-test9 (Var a0) (Var a1) = do
-  Var x <- switch a0 $ args
-             (match $ \(Foo a b) ->
-                switch a1 $ args
-                  (match $ \(Var y) -> unit y))
-  unit x
-
-runTest = interpret $ (test 5 :: Grin GrinValue)
-
-runTest' = interpret $ (test' 5)
-
-runTest2 = interpret $ (test2 5 6)
-
-runTest3 = interpret $ (test3 :: Grin GrinValue)
-
-runTest4 = interpret $ (test4 :: Grin GrinValue)
-
-runTest4' = interpret $ (test4' :: Grin GrinValue)
-
-runTest5 = interpret $ (test5 :: Grin GrinValue)
-
-runTest6 = interpret $ (test6 :: Grin GrinValue)
-
-runTest7 = interpret $ (test7 :: Grin GrinValue)
-
-runTest6' = declare (test6 :: Grin GrinValue)
-
-runTest8 = declare (test8 :: Var -> Grin GrinValue)
-
-runTest9 = declare (test9 :: Var -> Var -> Grin GrinValue)
