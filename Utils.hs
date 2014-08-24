fabc473d028faac4aa402a113267ded4911e9d21
Andreas Farre <andreas.farre@gmail.com>
2014-08-24 13:19:03 +0200

    Add the concept of node tags.

--- Utils.hs
+++ Utils.hs
@@ -2,11 +2,13 @@ module Utils where
 
 import Syntax
 
+newtype Tag = T { fromTag :: Value }
+
 var :: String -> Value
 var = Variable . VariableName . Name
 
-cnode :: String -> [Value] -> Value
-cnode = Node . var
+cnode :: Tag -> [Value] -> Value
+cnode = Node . fromTag
 
 vname :: String -> [Value] -> Value
 vname = Node . var
